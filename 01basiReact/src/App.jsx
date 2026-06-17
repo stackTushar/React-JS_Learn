dbff57eb6643933bfa1ed692839e993b70928a93
stackTushar <gupta.sanjay4729@gmail.com>
2026-06-17 15:39:04 +0530

    Learned Up three videos

---
+++ 01basiReact/src/App.jsx
@@ -0,0 +1,8 @@
+import Chai from "./chai";
+
+function App() {
+  // return <h1>Chai aur React with vite | Tushar</h1>;
+  return <Chai />;
+}
+
+export default App;
---
+++ 01basiReact/src/chai.jsx
@@ -0,0 +1,5 @@
+function Chai() {
+  return <h2>chai with Tushar</h2>;
+}
+
+export default Chai;
